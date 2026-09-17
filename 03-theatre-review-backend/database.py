8c01ab9f14eabe11b86f37a31e7ac0ca9123fc2f
krishnay-11 <yadavk649@gmail.com>
2026-09-17 17:07:59 +0530

    Completed 3 mini fastAPI projects

---
+++ 03-theatre-review-backend/database.py
@@ -0,0 +1,13 @@
+from sqlmodel import SQLModel, Session, create_engine
+
+DATABASE_URL = "sqlite:///reviews.db"
+
+engine = create_engine(DATABASE_URL, echo=True)
+def create_tables():
+    """Create all tables defined by SQLModel Class"""
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    """Dependency that provides a database session per request"""
+    with Session(engine) as session:
+        yield session
---
+++ 03-theatre-review-backend/main.py
@@ -0,0 +1,25 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from database import create_tables
+from routes.reviews import router as reviews_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Lifespan started")
+    create_tables()
+    print("Database tables created")
+    yield
+    # shutdown: cleanup here
+    print("Shutting down the app")
+
+app = FastAPI(
+    title="Theatre Reviews API",
+    description="Theatre reviews API backend",
+    lifespan=lifespan
+)
+
+app.include_router(reviews_router)
+@app.get("/")
+def root():
+    return {"message": "Welcome to Theatre Review API"}
